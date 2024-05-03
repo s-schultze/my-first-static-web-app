0415eee90be34ac3d2756eac74ea53c7d08c4724
s-schultze <s_schultze@live.de>
2024-05-03 17:15:21 +0200

    N12

--- src/js/hallo.js
+++ src/js/hallo.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Funktion, um den Text "Hallo" in der Textbox anzuzeigen
+    // Array mit den Texten, die angezeigt werden sollen
+    var texts = ["Hallo", "Anja", "Steffen", "Hallo"];
+    // Zähler, um den aktuellen Index im Array zu verfolgen
+    var counter = 0;
+  
+    // Funktion, um den nächsten Text aus dem Array anzuzeigen
     function displayText() {
       var textbox = document.getElementById("textbox");
-      textbox.value = "Hallo";
+      // Text aus dem Array anzeigen und den Zähler aktualisieren
+      textbox.value = texts[counter];
+      counter = (counter + 1) % texts.length; // Umschalten zum nächsten Text, wobei der Index innerhalb der Array-Grenzen bleibt
     }
   
     // Den Button auswählen und ihm einen Eventlistener hinzufügen
